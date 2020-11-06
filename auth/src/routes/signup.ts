1b58b89f5ad818149fe6ab8d3d24411d1479b395
AmrAbdalrahman <amr.abdalrahman94@gmail.com>
2020-11-06 18:40:25 +0200

    init use cookie and jwt and share jwt between through kubectl

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,5 +1,7 @@
 import express, {NextFunction, Request, Response} from 'express';
 import {check, validationResult} from 'express-validator';
+import jwt from 'jsonwebtoken';
+
 import {User} from "../models/user";
 import {RequestValidationError} from '../errors/request-validation-error';
 import {BadRequestError} from "../errors/bad-request-error";
@@ -18,6 +20,7 @@ router.post(
             .withMessage('Password must be between 4 and 20 characters')
     ],
     async (req: Request, res: Response, next: NextFunction) => {
+
         const errors = validationResult(req);
 
         if (!errors.isEmpty()) {
@@ -34,6 +37,17 @@ router.post(
 
         const user = User.build({email, password});
         await user.save();
+
+        //Generate JWT
+        const userJwt = jwt.sign({
+            id: user.id,
+            email: user.email
+        }, process.env.JWT_KEY!);
+
+        //Store it on session object
+        req.session = {
+            jwt: userJwt
+        };
 
         res.status(201).send(user);
     }
