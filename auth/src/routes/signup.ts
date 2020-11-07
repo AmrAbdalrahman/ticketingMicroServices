de46407193e738e9cd675aaa360539810c59d883
AmrAbdalrahman <amr.abdalrahman94@gmail.com>
2020-11-07 13:24:57 +0200

    signin implemention

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,10 +1,11 @@
 import express, {NextFunction, Request, Response} from 'express';
-import {check, validationResult} from 'express-validator';
+import {check} from 'express-validator';
 import jwt from 'jsonwebtoken';
 
+import {validateRequest} from "../middlewares/validate-request";
 import {User} from "../models/user";
-import {RequestValidationError} from '../errors/request-validation-error';
 import {BadRequestError} from "../errors/bad-request-error";
+
 
 const router = express();
 
@@ -19,13 +20,8 @@ router.post(
             .isLength({min: 4, max: 20})
             .withMessage('Password must be between 4 and 20 characters')
     ],
+    validateRequest,
     async (req: Request, res: Response, next: NextFunction) => {
-
-        const errors = validationResult(req);
-
-        if (!errors.isEmpty()) {
-            throw new RequestValidationError(errors.array());
-        }
 
         const {email, password} = req.body;
 
