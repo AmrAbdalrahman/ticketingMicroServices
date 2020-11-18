b32c1eae7fdc687c048d62cfa9988c41674da221
AmrAbdalrahman <amr.abdalrahman94@gmail.com>
2020-11-18 21:37:11 +0200

    tickets service listen for created and canceled order and testing them

--- tickets/src/routes/update.ts
+++ tickets/src/routes/update.ts
@@ -4,7 +4,7 @@ import {
     validateRequest,
     NotFoundError,
     requireAuth,
-    NotAuthorizedError,
+    NotAuthorizedError, BadRequestError,
 } from '@aaatickets/common';
 import {Ticket} from '../models/ticket';
 import {TicketUpdatedPublisher} from "../events/publishers/ticket-updated-publisher";
@@ -27,6 +27,10 @@ router.put(
 
         if (!ticket) {
             throw new NotFoundError();
+        }
+
+        if (ticket.orderId) {
+            throw new BadRequestError('Cannot edit a reserved ticket');
         }
 
         if (ticket.userId !== req.currentUser!.id) {
