8b422b2380b917ff845436f45393ce69ea233a2d
AmrAbdalrahman <amr.abdalrahman94@gmail.com>
2020-11-20 14:55:42 +0200

    payments service orders listeners and testing them and nats-wrapper return void promise ts

--- payments/src/index.ts
+++ payments/src/index.ts
@@ -1,13 +1,15 @@
 import mongoose from 'mongoose';
-import {app} from "./app";
-import {natsWrapper} from "./nats-wrapper";
+import { app } from './app';
+import { natsWrapper } from './nats-wrapper';
+import { OrderCancelledListener } from './events/listeners/order-cancelled-listener';
+import { OrderCreatedListener } from './events/listeners/order-created-listener';
 
 const start = async () => {
     if (!process.env.JWT_KEY) {
-        throw new Error('JWT_KEY must be defined!');
+        throw new Error('JWT_KEY must be defined');
     }
     if (!process.env.MONGO_URI) {
-        throw new Error('MONGO_URI must be defined!');
+        throw new Error('MONGO_URI must be defined');
     }
     if (!process.env.NATS_CLIENT_ID) {
         throw new Error('NATS_CLIENT_ID must be defined');
@@ -25,7 +27,6 @@ const start = async () => {
             process.env.NATS_CLIENT_ID,
             process.env.NATS_URL
         );
-
         natsWrapper.client.on('close', () => {
             console.log('NATS connection closed!');
             process.exit();
@@ -33,20 +34,22 @@ const start = async () => {
         process.on('SIGINT', () => natsWrapper.client.close());
         process.on('SIGTERM', () => natsWrapper.client.close());
 
+        new OrderCreatedListener(natsWrapper.client).listen();
+        new OrderCancelledListener(natsWrapper.client).listen();
+
         await mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
-            useCreateIndex: true
+            useCreateIndex: true,
         });
         console.log('Connected to MongoDb');
     } catch (err) {
-        console.log(err);
+        console.error(err);
     }
+
     app.listen(3000, () => {
-        console.log('Listening on port 3000');
+        console.log('Listening on port 3000!!!!!!!!');
     });
 };
 
 start();
-
-
